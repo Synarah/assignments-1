90e61ab95f167e59c86479168808c6a4e4d968f1
Synarah Sitaf <synarahsitaf@synarahs-mbp-2.lan>
2022-10-06 15:38:07 -0400

    A04

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -4,11 +4,37 @@
 #include "read_ppm.h"
 
 // Choose *one* to implement
-
+  
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
-}
+  char str[1068];
+  char check[2];
+  FILE* fp = fopen(filename, "rb");
+  if(fp == NULL){
+    //printf("Not a valid file");
+    return NULL;
+  }
+  fgets(str,1068,fp);
+  sscanf(str, " %s", check);
+  if(check[0] != 'P' || check[1] != '6'){
+    //printf("Not a valid file");
+    return NULL;
+  }
 
+  fgets(str,1068,fp);
+  sscanf(str, " %d %d", w,h);
+  struct ppm_pixel* arr = malloc(sizeof(struct ppm_pixel) * (*w)*(*h));
+  
+  
+  for(int i = 0; i < *h; i++){
+    for(int j = 0; j < *w; j++){
+      fscanf(fp, " %hhu %hhu %hhu", &arr[i*(*h) + j].red,&arr[i*(*h) + j].green,&arr[i*(*h) + j].blue);
+    }
+  }
+  fclose(fp); 
+  return(arr);
+}
+/*
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
   return NULL;
 }
+*/
--- A04/test_ppm.c
+++ A04/test_ppm.c
@@ -4,6 +4,24 @@
 
 int main() {
   // load feep-raw and print out the grid of pixels
+  int w;
+  int h;
+
+  struct ppm_pixel* ppm_array = read_ppm("feep-raw.ppm",&w,&h);
+  if(ppm_array == NULL){
+    printf("Not a valid file");
+  }
+  else{
+    printf("Testing file feep-raw.ppm: %d %d", w,h);
+  for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+      printf("(%hhu,%hhu,%hhu) ", ppm_array[i*h + j].red, ppm_array[i*h + j].green,ppm_array[i*h + j].blue);
+    }
+    printf("\n");
+  }
+  free(ppm_array);
+  }
+  
   return 0;
 }
 
